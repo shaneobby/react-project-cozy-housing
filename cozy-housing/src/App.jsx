430b84f0749a4f333b93a55c87b44c8cd3b2dac9
shaneobby <163554908+shaneobby@users.noreply.github.com>
2026-01-05 06:31:03 +0530

    .

--- cozy-housing/src/App.jsx
+++ cozy-housing/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import propertyInfo from './properties.json'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { HashRouter, Routes, Route} from 'react-router-dom'
 import Footer from './components/footer'
 import Header from './components/header'
 import Home from './pages/home'
@@ -9,7 +9,7 @@ import ExpandedProperty from './pages/ExpandedProperty'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route
@@ -22,7 +22,7 @@ function App() {
         />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
